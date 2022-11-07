566fe864e92e408085ae16686b63616e6977e9f7
oTuff <oscartuff@protonmail.com>
2022-11-07 10:25:09 +0100

    initial commit

---
+++ src/main/java/facades/Populator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import javax.persistence.EntityManagerFactory;
+import utils.EMF_Creator;
+
+/**
+ *
+ * @author tha
+ */
+public class Populator {
+    public static void populate(){
+        //EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+        // Import facade and persist some data into the DB
+
+    }
+    
+    public static void main(String[] args) {
+        populate();
+    }
+}
---
+++ src/test/java/facades/UserFacadeTest.java
@@ -0,0 +1,6 @@
+package facades;
+
+public class UserFacadeTest {
+
+    //
+}
